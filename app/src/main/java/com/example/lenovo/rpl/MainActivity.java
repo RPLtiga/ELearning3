41568eaf762f91dbad42f6b739a856095446f9af
syafaatadi <aatadinugraha@gmail.com>
2018-10-17 11:22:35 +0700

    Perbaikan tombol Masuk

--- app/src/main/java/com/example/lenovo/rpl/MainActivity.java
+++ app/src/main/java/com/example/lenovo/rpl/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.lenovo.rpl;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonmasuk;
+    ImageView logouin;
+    Animation frombottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonmasuk = (Button) findViewById(R.id.buttonmasuk);
+        buttonmasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent login = new Intent(MainActivity.this,login.class);
+            startActivity(login);
+            }
+        });
+
+        logouin= (ImageView) findViewById(R.id.logouin);
+        frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        logouin.setAnimation(frombottom);
     }
 }

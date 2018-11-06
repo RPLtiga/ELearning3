846cfde04acaac5b9825a2f45c48cbae55e51b40
ulfamulya <ulfamly177@gmail.com>
2018-11-06 15:16:15 +0700

    profile

--- app/src/main/java/com/example/lenovo/rpl/ProfileActivity.java
+++ app/src/main/java/com/example/lenovo/rpl/ProfileActivity.java
@@ -3,11 +3,11 @@ package com.example.lenovo.rpl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class dashboard extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_profile);
     }
 }
